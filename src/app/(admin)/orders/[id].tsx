2e755a8cff879d86c3cf4697d813541e7226caac
Ujjawal Kaul <ujjwal171@outlook.com>
2024-06-03 00:54:37 +0530

    Added order status update

--- src/app/(admin)/orders/[id].tsx
+++ src/app/(admin)/orders/[id].tsx
@@ -3,7 +3,9 @@ import OrderItemListItem from '@components/OrderItemListItem';
 import OrderListItem from '@components/OrderListItem';
 import { Stack, useLocalSearchParams } from 'expo-router';
 import React from 'react';
-import { FlatList, StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View, Pressable } from 'react-native';
+import Colors from '@/constants/Colors';
+import { OrderStatusList } from '@/types';
 
 export default function OrderDetailsScreen() {
   const { id } = useLocalSearchParams();
@@ -24,6 +26,39 @@ export default function OrderDetailsScreen() {
         contentContainerStyle={{ gap: 10 }}
         ListHeaderComponent={() => {
           return <OrderListItem order={order} />;
+        }}
+        ListFooterComponent={() => {
+          return (
+            <>
+              <Text style={{ fontWeight: 'bold' }}>Status</Text>
+              <View style={{ flexDirection: 'row', gap: 5 }}>
+                {OrderStatusList.map((status) => (
+                  <Pressable
+                    key={status}
+                    onPress={() => console.warn('Update status')}
+                    style={{
+                      borderColor: Colors.light.tint,
+                      borderWidth: 1,
+                      padding: 10,
+                      borderRadius: 5,
+                      marginVertical: 10,
+                      backgroundColor:
+                        order.status === status
+                          ? Colors.light.tint
+                          : 'transparent',
+                    }}>
+                    <Text
+                      style={{
+                        color:
+                          order.status === status ? 'white' : Colors.light.tint,
+                      }}>
+                      {status}
+                    </Text>
+                  </Pressable>
+                ))}
+              </View>
+            </>
+          );
         }}
       />
     </View>
