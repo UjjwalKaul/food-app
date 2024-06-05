cd0f45e5865460daa5082a90fb5846f729174fb7
Ujjawal Kaul <ujjwal171@outlook.com>
2024-06-05 20:55:04 +0530

    Added supabase dependencies

---
+++ src/lib/supabase.ts
@@ -0,0 +1,28 @@
+import 'react-native-url-polyfill';
+import * as SecureStore from 'expo-secure-store';
+import { createClient } from '@supabase/supabase-js';
+
+const ExpoSecureStoreAdapter = {
+  getItem: (key: string) => {
+    return SecureStore.getItemAsync(key);
+  },
+  setItem: (key: string, value: string) => {
+    SecureStore.setItemAsync(key, value);
+  },
+  removeItm: (key: string) => {
+    SecureStore.deleteItemAsync(key);
+  },
+};
+
+const supabaseUrl = 'https://spvgmioxttnswplmqbov.supabase.co';
+const supabaseAnonKey =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNwdmdtaW94dHRuc3dwbG1xYm92Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTc1MTUxNjksImV4cCI6MjAzMzA5MTE2OX0.1qRhwIneve_bUCRChmsGHnOpTrb9xj6UxlkOWpUrKb0';
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: ExpoSecureStoreAdapter as any,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+});
