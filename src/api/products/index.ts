12c893c9c25faa00e92bef40325dbf432c699349
Ujjawal Kaul <ujjwal171@outlook.com>
2024-06-06 23:30:26 +0530

    Added react query to get data

---
+++ src/api/products/index.ts
@@ -0,0 +1,14 @@
+import { supabase } from '@/lib/supabase';
+import { useQuery } from '@tanstack/react-query';
+export default function useProductList() {
+  return useQuery({
+    queryKey: ['products'],
+    queryFn: async () => {
+      const { data, error } = await supabase.from('products').select('*');
+      if (error) {
+        throw new Error(error.message);
+      }
+      return data;
+    },
+  });
+}
